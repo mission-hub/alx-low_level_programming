1dc34523e8ad22d6465bf9a607760b11fee54716
mission-hub <molokwumirian19@gmail.com>
2023-03-18 21:46:33 -0700

    good

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - My first C program
+ * Return: 0(Successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}}
+
