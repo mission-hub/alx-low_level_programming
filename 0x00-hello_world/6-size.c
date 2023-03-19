b69aeab6972e3d38f824af458f8a53811d7809fb
mission-hub <molokwumirian19@gmail.com>
2023-03-19 00:46:52 -0700

    good

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *
+ * main- print data types in c
+ *
+ * Return= 0
+ */
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("size of a char: %lu byte(S)\n", (unsigned long)size of(a));
+	printf("size of a int: %lu byte(S)\n", (unsigned long)size of(b));
+	printf("size of a long int: %lu byte(S)\n", (unsigned long)size of(c));
+	printf("size of a long long int: %lu byte(S)\n", (unsigned long)size of(d);
+	printf("size of a float: %lu byte(S)\n", (unsigned long)size of(e);
+	return (0);
+}
