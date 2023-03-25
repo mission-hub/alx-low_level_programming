bb71d1ad0016afe8b50bd071ec44030f7cc56112
mission-hub <molokwumirian19@gmail.com>
2023-03-24 17:42:34 -0700

    go

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,8 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 void positive_or_negative(int i);
-{
-	if(i > 0)
-		printf("%d is positive\n", i);
-	else if (i < 0)
-		printf("%d is negative\n", i);
-	else
-			printf("%d is zero\n", i);
-}
+
+#endif /* MAIN_H */
